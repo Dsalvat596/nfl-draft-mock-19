c665ad5c835a9464a6ce8c1e5026081e1b13caf8
dsalvat596 <dsalvat596@gmail.com>
2019-04-13 15:22:46 -0400

    table populated w/ pagination and sort

--- src/app/prospects/prospect-area/prospect-area.component.ts
+++ src/app/prospects/prospect-area/prospect-area.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProspectsService } from 'src/app/services/prospects.service';
 import { Prospect } from 'src/app/models/prospect';
 import { Team } from 'src/app/models/team';
 import { TeamsService } from 'src/app/services/teams.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-prospect-area',
@@ -12,23 +15,27 @@ import { TeamsService } from 'src/app/services/teams.service';
 export class ProspectAreaComponent implements OnInit {
   currentTeam: Team;
   displayedColumns: string[] = ['ovr_rank', 'last_name', 'first_name', 'position', 'pos_rank', 'school', 'year', 'height', 'weight'];
-  dataSource: Prospect[];
-
+  dataSource: MatTableDataSource<Prospect>;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   constructor(private ps: ProspectsService, private ts: TeamsService) { }
 
   ngOnInit() {
     this.ps.getProspects();
     this.ps.availableProspectsUpdated.subscribe((res)=>{
-      this.dataSource = res.sort(function(a,b){
+      let datatemp = res.sort(function(a,b){
         if(a.ovr_rank < b.ovr_rank){return -1}
         if(a.ovr_rank > b.ovr_rank){return 1}
         return 0;
       })
+      this.dataSource = new MatTableDataSource(datatemp); 
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
       console.log(this.dataSource[3]);
     })
     this.ts.currentTeamUpdated.subscribe((team:Team)=>{
       this.currentTeam = team;
-      console.log(this.currentTeam.name);
+      console.log(this.currentTeam);
     })
   }
 
